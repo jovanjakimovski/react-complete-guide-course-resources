7e4dc72cf9736ec62364da1743e3dfd6e15f0051
Jovan Jakimovski <jovanj9730@gmail.com>
2025-09-25 13:10:24 +0200

    Refactored code into multiple componenets

--- code/03 React Essentials/06-alternative-props-syntax/src/App.jsx
+++ code/03 React Essentials/06-alternative-props-syntax/src/App.jsx
@@ -1,36 +1,9 @@
-import reactImg from './assets/react-core-concepts.png';
+
 import { CORE_CONCEPTS } from './data.js';
+import CoreConcept from './components/CoreConcept.jsx';
+import Header from './components/Header.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -47,7 +20,7 @@ function App() {
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[4]} />
           </ul>
         </section>
       </main>
